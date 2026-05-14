25843d5789de17291ac7081c014f25306bd197e5
Shaurya <tiwarishaurya395@gmail.com>
2026-05-14 16:53:49 +0530

    feat: add styles for PageNotFound component

--- client/src/components/PageNotFound/PageNotFound.js
+++ client/src/components/PageNotFound/PageNotFound.js
@@ -1,35 +1,43 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import './PageNotFound.scss'
+import error404 from '../../assets/gif/error404.gif'
 
-import pagenotfound from '../../assets/gif/pagenotfound.gif'
+const PageNotFound = () => {
+  useEffect(() => {
+    document.title = 'Prime Video: Page Not Found';
+  }, []);
 
-const PageNotFound = ()=>{
-    return (
-        <div>
-            <Link 
-                to='/' 
-                style={
-                    {   
-                        color :'white',
-                        display : 'block',
-                        fontSize:'2rem',
-                        textDecoration : 'none',
-                        position: 'fixed',
-                        zIndex : '2',
-                        top : '20px',
-                        right:'10%'
-                    }
-                } 
-            
-            >
-                Home
-            </Link>
-            <img 
-                src={pagenotfound}
-                style={{width:'100vw',height:'100vh',objectFit:'contain',background:'black',position:'fixed'}}
-            ></img>
+  return (
+    <div className="pagenotfound-container">
+      <div className="pagenotfound-bg-accent"></div>
+      
+      <div className="pagenotfound-visual animate__animated animate__zoomIn">
+        <img src={error404} alt="404 Lost in Space" />
+      </div>
+
+      <div className="pagenotfound-content animate__animated animate__fadeInUp">
+        <h1>404</h1>
+        <h2>Oops! We can't find that page.</h2>
+        <p>
+          We're sorry, but the page you are looking for doesn't exist or has been moved. 
+          Don't worry, you can get back to the action by clicking below.
+        </p>
+
+        <div className="pagenotfound-actions">
+          <Link to="/" className="home-button">
+            Go to Home
+          </Link>
+          <Link to="/movies" className="browse-link">
+            Browse Movies
+          </Link>
+          <Link to="/tvshows" className="browse-link">
+            TV Shows
+          </Link>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default PageNotFound
